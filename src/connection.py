82802b9ef5e0f6bdca8ca2057be79d15ec572c01
alyssa le <169010345+alyssaqle@users.noreply.github.com>
2025-09-08 18:43:03 -0700

    update fixed test_session and connection setup

--- src/connection.py
+++ src/connection.py
@@ -10,10 +10,10 @@ def get_session():
         "account": os.getenv("SNOWFLAKE_ACCOUNT"),
         "user": os.getenv("SNOWFLAKE_USER"),
         "password": os.getenv("SNOWFLAKE_PASSWORD"),
-        "role": os.getenv("SNOWFLAKE_ROLE", "ANALYST"),
-        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE", "COMPUTE_WH"),
-        "database": os.getenv("SNOWFLAKE_DATABASE", "SKYTRAX_REVIEWS_DB"),
-        "schema": os.getenv("SNOWFLAKE_SCHEMA", "MARTS"),
+        "role": os.getenv("SNOWFLAKE_ROLE"),
+        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+        "database": os.getenv("SNOWFLAKE_DATABASE"),
+        "schema": os.getenv("SNOWFLAKE_SCHEMA"),
     }
     return Session.builder.configs(params).create()
 
